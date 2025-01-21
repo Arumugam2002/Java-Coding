f829a334a828eb6d07bd1c551817f8a82fafe30a
Arumugam2002 <arumugampandaram65@gmail.com>
2025-01-21 20:37:49 +0530

    TryCatchExample & Multiple Catch Example

--- ExceptionHandling/src/main/java/TryCatchBlockExample.java
+++ ExceptionHandling/src/main/java/TryCatchBlockExample.java
@@ -2,15 +2,30 @@ public class TryCatchBlockExample {
 
     public static void main(String[] args) {
 
-        String word = "Arumugam";
-        try{
-            int num = Integer.parseInt(word);
-            System.out.println(num);
-        }
-
-        catch (Exception e){
-            System.out.println("String cannot be converted to int as the error is " + e);
-        }
-
+        parseString("20");
     }
+        public static void parseString(String numberString)
+        {
+            try{
+                //System.out.println(numberString.length());
+                int num = Integer.parseInt(numberString);
+                System.out.println(num);
+            }
+
+            catch (NumberFormatException | NullPointerException e){
+                System.out.println("String cannot be converted to int");
+            }
+
+            catch (Exception e)
+            {
+                System.out.println(e);
+            }
+
+            finally {
+                System.out.println("This statement will be printed whenever the code is being compiled");
+            }
+        }
+
+
+
 }
